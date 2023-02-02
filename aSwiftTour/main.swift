832c3ab6be417560dc914a71cc541961dd947492
f2rkan <furkanrolla@icloud.com>
2023-02-02 20:52:37 +0300

    a swift tour

--- aSwiftTour/main.swift
+++ aSwiftTour/main.swift
@@ -7,5 +7,12 @@
 
 import Foundation
 
-print("Hello, World!")
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+// Prints "false"
 
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
